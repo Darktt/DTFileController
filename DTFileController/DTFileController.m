1c137fdb2901739f01488766154246f1ff921a11
Darktt <pippipig@gmail.com>
2014-08-06 15:24:47 +0800

    Reverse last version.

--- DTFileController/DTFileController.m
+++ DTFileController/DTFileController.m
@@ -1,13 +1,13 @@
 // DTFileController.m
-// 
+//
 // Copyright (c) 2013 Darktt
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -120,7 +120,11 @@ static DTFileController *singleton = nil;
 {
     NSNumber *currentSpace = [self checkStorageSpace];
     
-//    NSLog(@"Free : %@, File Size : %@", currentSpace, size);
+#ifdef DEBUG_MODE
+    
+    NSLog(@"Free : %@, File Size : %@", currentSpace, size);
+    
+#endif
     
     return ([currentSpace longLongValue] > [size longLongValue]) ? YES : NO;
 }
@@ -153,7 +157,7 @@ static DTFileController *singleton = nil;
 #endif
     
     NSString *documentPath = [paths objectAtIndex:0];
-
+    
     return documentPath;
 }
 
@@ -175,7 +179,7 @@ static DTFileController *singleton = nil;
 #endif
     
     NSString *cachesPath = [paths objectAtIndex:0];
-
+    
     return cachesPath;
 }
 
@@ -216,7 +220,7 @@ static DTFileController *singleton = nil;
     NSLog(@"Read Path: %@",filePath);
     
 #endif
-
+    
     if ([self fileExistAtPath:filePath]) {
         NSString *string = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
         return string;
@@ -302,7 +306,7 @@ static DTFileController *singleton = nil;
 
 - (BOOL)createDirectoryUnderDocumentWithDirectoryName:(NSString *)directory
 {
-
+    
     NSString *folderPathUnderDocument = [[self documentPath] stringByAppendingPathComponent:directory];
     
 #ifdef DEBUG_MODE
@@ -407,29 +411,18 @@ static DTFileController *singleton = nil;
 
 #pragma mark - Get File List In Directory
 
-- (NSArray *)filesOfCurrentDirectoryName:(NSString *)directoryName skipHiddenFile:(BOOL)skipHiddenFile
+- (NSArray *)filesOfCurrentDirectoryName:(NSString *)directoryName
 {
     NSString *path = [self currentApplicationPathWithFileName:directoryName];
-    NSArray *files = [self filesWithDirectoryPath:path skipHiddenFile:skipHiddenFile];
+    NSArray *files = [self filesWithDirectoryPath:path];
     
     return files;
 }
 
-- (NSArray *)filesWithDirectoryPath:(NSString *)path skipHiddenFile:(BOOL)skipHiddenFile
+- (NSArray *)filesWithDirectoryPath:(NSString *)path
 {
     NSError *error = nil;
-    NSArray *files = nil;
-    
-    if (skipHiddenFile) {
-        
-        NSURL *fileURL = [NSURL fileURLWithPath:path];
-        files = [[NSFileManager defaultManager] contentsOfDirectoryAtURL:fileURL includingPropertiesForKeys:@[NSURLNameKey] options:NSDirectoryEnumerationSkipsHiddenFiles error:&error];
-        
-    } else {
-        
-        files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&error];
-        
-    }
+    NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&error];
     
     if (error != nil) {
         NSLog(@"%s **Error**: %@", __func__, error);
@@ -519,7 +512,7 @@ static DTFileController *singleton = nil;
     }
     
     QueueBlock copyQueueBlock = ^(){
-    
+        
         NSUInteger offset = 0;
         NSUInteger chunkSize = 1024 * 100;
         long long size = [[self getFileInformationAtPath:path] fileSize];
